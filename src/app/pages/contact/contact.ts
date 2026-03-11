b13cd3b6276cfc3ea14a9a428e4ef954df9c7ce6
Mohammadaburizq1 <mohammadsaburizq@gmail.com>
2026-03-12 01:29:38 +0300

    foorer and contact and servcices

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+  private fb = inject(FormBuilder);
+
+  contactForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    service: ['', Validators.required],
+    message: [''],
+  });
+
+  submit() {
+    if (this.contactForm.invalid) return;
+
+    console.log(this.contactForm.value);
+
+    // later connect to API
+    // this.contactService.sendMessage(...)
+  }
+}
--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -6,4 +6,35 @@ import { Component } from '@angular/core';
   templateUrl: './services.html',
   styleUrl: './services.scss',
 })
-export class Services {}
+export class Services {
+  services = [
+    {
+      title: 'House Cleaning',
+      description: 'Complete home cleaning for kitchens, bedrooms, bathrooms, and living areas.',
+    },
+    {
+      title: 'Deep Cleaning',
+      description:
+        'Detailed cleaning for neglected spaces, seasonal resets, or move-in/move-out needs.',
+    },
+    {
+      title: 'Office Cleaning',
+      description:
+        'Professional workspace cleaning to keep your office clean, safe, and presentable.',
+    },
+    {
+      title: 'Bathroom Cleaning',
+      description: 'Disinfection and deep scrubbing of sinks, toilets, tiles, mirrors, and floors.',
+    },
+    {
+      title: 'Kitchen Cleaning',
+      description:
+        'Surface sanitizing, appliance exterior cleaning, counters, sinks, and cabinet wipe-down.',
+    },
+    {
+      title: 'Custom Plans',
+      description:
+        'Flexible cleaning packages based on your schedule, property type, and service needs.',
+    },
+  ];
+}
